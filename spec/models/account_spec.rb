c2b76f0cb119eb027e6ecaa5950a3b0f44c0afd0
don-kakashi <idriss0said0alaoui@gmail.com>
2018-02-23 17:23:30 +0000

    Update rails_helper with Should_Matcher configurations

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -16,9 +16,9 @@ describe Account do
   it { should validate_presence_of(:password) }
   it { should validate_presence_of(:password_confirmation) }
 
-  it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
-  it { should allow_value('example@domain.com').for(email) }
+
+  it { should allow_value('example@domain.com').for(:email) }
 
   it { should validate_confirmation_of(:password) }
 
