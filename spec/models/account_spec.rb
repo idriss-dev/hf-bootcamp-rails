fc29f699ee4267b4ffe8c7f8405b66ca4418f555
don-kakashi <idriss0said0alaoui@gmail.com>
2018-02-23 17:16:09 +0000

    Adds Spec for User Model

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Account do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @account = FactoryBot.build(:account) }
+  it "has a valid factory" do
+    expect(build(:factory_you_built)).to be_valid
+  end
+
+  subject { @account }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:password_confirmation) }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should allow_value('example@domain.com').for(email) }
+
+  it { should validate_confirmation_of(:password) }
+
+  it { should be_valid }
 end
