b7b9e063d5ddb9418b2e4e15858a9e8320383662
don-kakashi <idriss0said0alaoui@gmail.com>
2018-02-26 11:51:56 +0000

    Adds account resources in the routing config

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :objectives
   resources :admins
   resources :users
+  resources :account
+
   post 'user_token' => 'account_token#create'
 
   # TO DO list of ressources of the project Objectives, Organizations, Departments
