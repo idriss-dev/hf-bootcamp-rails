693d14ebed1853c317451e2ea74719c96293ebbc
don-kakashi <idriss0said0alaoui@gmail.com>
2018-02-26 10:38:32 +0000

    Updates Specs for account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,10 +4,6 @@ class Account < ApplicationRecord
   validates_length_of       :password, maximum: 72, minimum: 8, allow_nil: true, allow_blank: false
   validates_confirmation_of :password, allow_nil: true, allow_blank: false
 
-  before_validation {
-    (self.email = self.email.to_s.downcase)
-  }
-
   validates_presence_of     :email
   validates_uniqueness_of   :email
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Account do
   before { @account = FactoryBot.build(:account) }
+
   it "has a valid factory" do
-    expect(build(:factory_you_built)).to be_valid
+    expect(build(:account)).to be_valid
   end
 
   subject { @account }
@@ -11,15 +12,15 @@ describe Account do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:password_digest) }
 
   it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password) }
-  it { should validate_presence_of(:password_confirmation) }
-
   it { should validate_uniqueness_of(:email) }
 
-  it { should allow_value('example@domain.com').for(:email) }
+  it { should validate_length_of(:password).is_at_most(72) }
+  it { should validate_length_of(:password).is_at_least(8) }
 
+  it { should allow_value('example@domain.com').for(:email) }
   it { should validate_confirmation_of(:password) }
 
   it { should be_valid }
