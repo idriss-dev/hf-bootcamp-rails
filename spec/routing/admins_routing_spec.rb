e2b4b9f759f4492bb1d7a864427e699f18ebb95f
don-kakashi <idriss0said0alaoui@gmail.com>
2018-02-26 11:46:40 +0000

    Updates rails_helper to spec helper

--- spec/routing/admins_routing_spec.rb
+++ spec/routing/admins_routing_spec.rb
@@ -1,4 +1,4 @@
-require "rails_helper"
+require "spec_helper"
 
 RSpec.describe AdminsController, type: :routing do
   describe "routing" do
--- spec/routing/authentication_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe AuthenticationController, type: :routing do
-  describe 'POST #auth' do
-    it 'routes to POST auth to user_token#create' do
-      expect(post :auth).to route_to('user_token#create')
-    end
-  end
-end
