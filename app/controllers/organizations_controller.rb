1bdf2d10e67dc75459882b09350a05d820af1c5b
don-kakashi <idriss0said0alaoui@gmail.com>
2018-03-12 14:09:12 +0000

    Adds Auth current_account before the user can have access to the organization endpoints

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,6 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :update, :destroy]
+  before_action :authenticate_account
 
 =begin
        @api {get} /organizations/ All
